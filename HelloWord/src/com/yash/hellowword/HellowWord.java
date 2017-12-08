32c79d96d688f8e814cd5c35d9edf98f0d205291
yashvantchauhan <yashvant.chauhan@gmail.com>
2017-12-08 07:41:41 +0530

    message

---
+++ HelloWord/src/com/yash/hellowword/HellowWord.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.yash.hellowword;
+
+/**
+ * @author yashvant
+ *
+ */
+public class HellowWord {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hi Hello!!");
+
+	}
+
+}
